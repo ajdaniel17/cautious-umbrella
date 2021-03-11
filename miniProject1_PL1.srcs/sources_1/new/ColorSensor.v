5b1c214a8ec27f7e74ef9bc8eabc1b9e927ce417
ajdaniel17 <ajdaniel17@yahoo.com>
2021-03-10 21:54:07 -0600

    I fucked it upp2

--- miniProject1_PL1.srcs/sources_1/new/ColorSensor.v
+++ miniProject1_PL1.srcs/sources_1/new/ColorSensor.v
@@ -22,6 +22,7 @@
 
 module ColorSensor(
     input clock,
+<<<<<<< HEAD
     input colorinput,
     output s2,s3,
     output LED0,LED1,LED2,LED3,LED4
@@ -141,6 +142,43 @@ module ColorSensor(
         green = 0;
         end
         else if (blueFRQ > redFRQ && blueFRQ > grnFRQ)
+=======
+    input colorinput,frqdone,
+    output s2,s3,
+    output LED0,LED1,LED2,LED3,LED4,
+    input[19:0] FRQ
+    );
+    
+    reg enable = 1;
+    reg temps2 = 1;
+    reg temps3 = 0;
+    reg [19:0] TEMPFRQ;
+    reg [1:0] colorsetting = 2'd0;
+    reg [19:0] TEMPRED,TEMPBLUE,TEMPGREEN,TEMPWHITE;
+    reg done = 1;
+    reg TLED0 = 0,TLED1 = 0,TLED2 = 0,TLED3 = 0;
+    reg[16:0] count; 
+    reg done1=0,done2=0;
+    
+ReadFrequency Readthis(
+     .CLK(clock),        
+     .enable(enable),       
+     .IN(colorinput),         
+     .freq(FRQ),
+     .done(frqdone) 
+     );
+     
+     always @(posedge clock)
+     begin
+        if(frqdone)
+        begin
+            TEMPFRQ = FRQ;
+            enable = 0;
+            done1 =1;    
+        end
+        
+        if (count > 1000)
+>>>>>>> parent of d3dc704 (Re)
         begin
         red = 0;
         blue = 1;
@@ -178,21 +216,21 @@ module ColorSensor(
                  temps2 = 0;
                  temps3 = 0;
                  colorsetting = 2'd1;
-                 FRQenable = 1;
+                 enable = 1;
                  end
                  2'd1 : begin //red to green
                  TEMPRED = TEMPFRQ;
                  temps2 = 1;
                  temps3 = 1;
                  colorsetting = 2'd2;
-                 FRQenable = 1;
+                 enable = 1;
                  end
                  2'd2 : begin //green to blue
                  TEMPGREEN = TEMPFRQ;
                  temps2 = 0;
                  temps3 = 1;
                  colorsetting = 2'd3;
-                 FRQenable = 1;
+                 enable = 1;
                  end
                  2'd3 : begin //blue to white
                  TEMPBLUE = TEMPFRQ;
@@ -208,19 +246,18 @@ module ColorSensor(
                  done2 = 0;
         end    
         
-        if(done4)
-            begin
-            done3 = 0;
-            DIVenable = 1;
-            end
-            
         if(~done)
 >>>>>>> parent of 4e1b925 (updagte)
         begin
+<<<<<<< HEAD
 
         end
+=======
+        enable = 0;
+>>>>>>> parent of d3dc704 (Re)
         
         
+<<<<<<< HEAD
         
         
         
@@ -234,4 +271,16 @@ module ColorSensor(
     assign LED4 = freqOFF;
     assign s2 = temps2;
     assign s3 = temps3;
+=======
+        done = 1;
+        enable = 1;
+        end
+     end
+assign LED0 = TLED0;
+assign LED1 = TLED1;
+assign LED2 = TLED2;
+assign LED3 = TLED3;
+assign s2 = temps2;
+assign s3 = temps3;
+>>>>>>> parent of d3dc704 (Re)
 endmodule
